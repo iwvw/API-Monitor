29ca515c22ff46dd661053bd8a731e4ba9db5322
iwvw <2285740204@qq.com>
2025-12-29 11:52:08 +0800

    fix: revert AMLL style import path to follow exports spec

--- src/js/modules/music.js
+++ src/js/modules/music.js
@@ -7,7 +7,7 @@ import { store } from '../store.js';
 import { toast } from './toast.js';
 
 // 导入 AMLL 样式
-import '@applemusic-like-lyrics/core/dist/amll-core.css';
+import '@applemusic-like-lyrics/core/style.css';
 
 // 音频播放器实例
 let audioPlayer = null;
